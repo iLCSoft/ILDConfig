fa2ffd857a2f1e1a6dc6c586ef8fab2f98495ae3
Jan Engels <jan.engels@desy.de>
2012-07-02 10:16:02 +0000

    copy stdout and stderr to tarball

--- StandardConfig/scripts/sim/job.sh
+++ StandardConfig/scripts/sim/job.sh
@@ -136,6 +136,9 @@ cleanup(){
     # job specific stuff
     cp -vf *.{sh,py,log,tgz,xml,tbl,steer,g4macro,in} $LOG_FILE_DIR
 
+    # copy stdout and stderr
+    cp -vf $JOB_STARTDIR/std{out,err} $LOG_FILE_DIR
+
     # no need to copy job-wrapper.sh and MokkaDBConfig.tgz
     rm -vf $LOG_FILE_DIR/job-wrapper.sh
     rm -vf $LOG_FILE_DIR/MokkaDBConfig.tgz
