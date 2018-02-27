d4265807c9ebe739b80e019dae44ed62836c4860
Ete Remi <remi.ete@gmail.com>
2018-02-27 18:31:36 +0100

    Added ILD_s5_v02 DDSim test

--- .travis-ci.d/test.sh
+++ .travis-ci.d/test.sh
@@ -5,18 +5,33 @@ source $ILCSOFT/init_ilcsoft.sh
 
 cd /Package/StandardConfig/production
 
-ddsim \
-  --inputFiles Examples/bbudsc_3evt/bbudsc_3evt.stdhep \
-  --outputFile bbudsc_3evt_SIM_test_ILD_l5_v02.slcio \
-  --compactFile $lcgeo_DIR/ILD/compact/ILD_l5_v02/ILD_l5_v02.xml \
-  --steeringFile ddsim_steer.py
+for detector in ILD_l5_v02 ILD_s5_v02
+do
+  echo "-- Running DDSim ${detector} ..."
+  ddsim \
+    --inputFiles Examples/bbudsc_3evt/bbudsc_3evt.stdhep \
+    --outputFile bbudsc_3evt_SIM_test_${detector}.slcio \
+    --compactFile $lcgeo_DIR/ILD/compact/${detector}/${detector}.xml \
+    --steeringFile ddsim_steer.py > travis-ci.log 2>&1
 
-ddsimStatus=$?
+  ddsimStatus=$?
 
-if [[ ddsimStatus != 0 ]]
-then
-  echo "DDSim ILD_l5_v02: 3 events test failed"
-  exit $ddsimStatus
-else
-  echo "-- DDSim ILD_l5_v02: passing !"
+  if [[ ddsimStatus != 0 ]]
+  then
+    cat travis-ci.log
+    echo "-- ERROR - DDSim ${detector}: 3 events test failed"
+    exit $ddsimStatus
+  else
+    echo "-- DDSim ${detector}: passing !"
+  fi
+  
+  if [ ! -f "bbudsc_3evt_SIM_test_${detector}.slcio" ]
+  then
+    ls -lthr
+    echo "-- ERROR - DDSim ${detector}: No output file found (bbudsc_3evt_SIM_test_${detector}.slcio)"
+  fi
 fi
+
+
+
+
