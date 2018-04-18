a3ad9e136e225b98b04fb05ccb768affe33dc40d
Ete Remi <remi.ete@gmail.com>
2018-04-18 12:54:13 +0200

    Split TravisCI jobs to run 1 sim and 1 reco per job. Currently 4 jobs will run for l5/s5 and o1/o2 models

--- .travis-ci.d/test.sh
+++ .travis-ci.d/test.sh
@@ -6,83 +6,73 @@ source $ILCSOFT/init_ilcsoft.sh
 cd /Package/StandardConfig/production
 
 ##
-## Test DDSim for our current detector models
+## Test DDSim
 ##
-for detector in ILD_l5_v02 ILD_s5_v02
+echo "-- Running DDSim ${SIM_MODEL} ..."
+ddsim \
+  --inputFiles Examples/bbudsc_3evt/bbudsc_3evt.stdhep \
+  --outputFile bbudsc_3evt_SIM_test_${SIM_MODEL}.slcio \
+  --compactFile $lcgeo_DIR/ILD/compact/${SIM_MODEL}/${SIM_MODEL}.xml \
+  --steeringFile ddsim_steer.py > travis-ci.log 2>&1
+
+ddsimStatus=$?
+
+if [[ $ddsimStatus != 0 ]]
+then
+  cat travis-ci.log
+  echo "-- ERROR - DDSim ${SIM_MODEL}: 3 events test failed"
+  exit $ddsimStatus
+else
+  echo "-- DDSim ${SIM_MODEL}: test passing !"
+fi
+
+# test presence of output file
+if [ ! -f "bbudsc_3evt_SIM_test_${SIM_MODEL}.slcio" ]
+then
+  ls -lthr
+  echo "-- ERROR - DDSim ${SIM_MODEL}: No output file found (bbudsc_3evt_SIM_test_${SIM_MODEL}.slcio)"
+  exit 1
+fi
+
+
+##
+## Test Marlin reconstruction
+##
+outputBaseName="bbudsc_3evt_RECNoBG_Test_${REC_MODEL}"
+
+echo "-- Running Marlin ${REC_MODEL} no bg ..."
+Marlin MarlinStdReco.xml \
+	--constant.lcgeo_DIR=$lcgeo_DIR \
+  --constant.DetectorModel=${REC_MODEL} \
+  --constant.OutputBaseName=${outputBaseName} \
+  --constant.RunBeamCalReco=false \
+  --global.LCIOInputFiles=bbudsc_3evt_SIM_test_${SIM_MODEL}.slcio > travis-ci.log 2>&1
+
+marlinStatus=$?
+
+if [[ $marlinStatus != 0 ]]
+then
+  cat travis-ci.log
+  echo "-- ERROR - Marlin ${REC_MODEL} no bg: 3 events test failed"
+  exit $marlinStatus
+else
+  echo "-- Marlin ${REC_MODEL} no bg: test passing !"
+fi
+
+# test presence of different output files
+checkFileList="${outputBaseName}_REC.slcio \
+               ${outputBaseName}_DST.slcio \
+               ${outputBaseName}_PfoAnalysis.root \
+               ${outputBaseName}_AIDA.root"
+for checkFile in ${checkFileList}
 do
-  echo "-- Running DDSim ${detector} ..."
-  ddsim \
-    --inputFiles Examples/bbudsc_3evt/bbudsc_3evt.stdhep \
-    --outputFile bbudsc_3evt_SIM_test_${detector}.slcio \
-    --compactFile $lcgeo_DIR/ILD/compact/${detector}/${detector}.xml \
-    --steeringFile ddsim_steer.py > travis-ci.log 2>&1
-
-  ddsimStatus=$?
-
-  if [[ $ddsimStatus != 0 ]]
-  then
-    cat travis-ci.log
-    echo "-- ERROR - DDSim ${detector}: 3 events test failed"
-    exit $ddsimStatus
-  else
-    echo "-- DDSim ${detector}: test passing !"
-  fi
-  
-  # test presence of output file
-  if [ ! -f "bbudsc_3evt_SIM_test_${detector}.slcio" ]
+  if [ ! -f ${checkFile} ]
   then
     ls -lthr
-    echo "-- ERROR - DDSim ${detector}: No output file found (bbudsc_3evt_SIM_test_${detector}.slcio)"
+    echo "-- ERROR - Marlin ${REC_MODEL} no bg: Missing output file ${checkFile}"
     exit 1
+  else
+    echo "-- Marlin ${REC_MODEL} no bg: ${checkFile} present ..."
   fi
 done
 
-
-##
-## Test Marlin reconstruction for our current detector models 
-##
-for largeOrSmall in l5 s5
-do
-  for detectorOption in o1 o2
-  do
-    simDetector="ILD_${largeOrSmall}_v02"
-    recDetector="ILD_${largeOrSmall}_${detectorOption}_v02"
-    outputBaseName="bbudsc_3evt_RECNoBG_Test_${recDetector}"
-    
-    echo "-- Running Marlin ${recDetector} no bg ..."
-    Marlin MarlinStdReco.xml \
-    	--constant.lcgeo_DIR=$lcgeo_DIR \
-      --constant.DetectorModel=${recDetector} \
-      --constant.OutputBaseName=${outputBaseName} \
-      --constant.RunBeamCalReco=false \
-      --global.LCIOInputFiles=bbudsc_3evt_SIM_test_${simDetector}.slcio > travis-ci.log 2>&1
-    
-    marlinStatus=$?
-
-    if [[ $marlinStatus != 0 ]]
-    then
-      cat travis-ci.log
-      echo "-- ERROR - Marlin ${recDetector} no bg: 3 events test failed"
-      exit $marlinStatus
-    else
-      echo "-- Marlin ${recDetector} no bg: test passing !"
-    fi
-    
-    # test presence of different output files
-    checkFileList="${outputBaseName}_REC.slcio \
-                   ${outputBaseName}_DST.slcio \
-                   ${outputBaseName}_PfoAnalysis.root \
-                   ${outputBaseName}_AIDA.root"
-    for checkFile in ${checkFileList}
-    do
-      if [ ! -f ${checkFile} ]
-      then
-        ls -lthr
-        echo "-- ERROR - Marlin ${recDetector} no bg: Missing output file ${checkFile}"
-        exit 1
-      else
-        echo "-- Marlin ${recDetector} no bg: ${checkFile} present ..."
-      fi
-    done
-  done
-done
