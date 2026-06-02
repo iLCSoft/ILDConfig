ab6a440405fefd6164bd9da65a3116b87db914d4
Victor Schwan <victor.schwan@cern.ch>
2026-06-02 18:50:00 +0200

    [FCC] Add custom FCC High Level Reco sequence

--- StandardConfig/production/ILDReconstruction.py
+++ StandardConfig/production/ILDReconstruction.py
@@ -167,6 +167,11 @@ parser.add_argument(
     help="Run the Silicon-TPC-track-merging for ILD@FCC-ee",
     action="store_true",
 )
+parser.add_argument(
+    "--doHLR",
+    help="FCC models: do High Level Reco",
+    action="store_true",
+)
 
 
 def get_compact_file_path(detector_model: str):
@@ -302,6 +307,8 @@ if not reco_args.trackingOnly:
 
     if not is_FCCee_model:
         sequenceLoader.load("HighLevelReco/HighLevelReco")
+    elif reco_args.doHLR:
+        sequenceLoader.load("HighLevelReco/HighLevelReco_FCCee")
 
     if not reco_args.noPFO:
         MyPfoAnalysis = MarlinProcessorWrapper("MyPfoAnalysis")
