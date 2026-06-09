81d33ac3d241b3b2a603012fbf17d285cc3213ce
Victor Schwan <victor.schwan@cern.ch>
2026-06-09 11:34:33 +0300

    Minor improvements

--- StandardConfig/production/Tracking/TrackingReco_FCCeeMDI.py
+++ StandardConfig/production/Tracking/TrackingReco_FCCeeMDI.py
@@ -36,7 +36,7 @@ MyClupatraProcessor.Parameters = {
     "TrackIsCurlerOmega": ["0.001"],
     "TrackStartsInnerDist": ["25"],
     "TrackSystemName": ["DDKalTest"],
-    "VXDHitCollection": ["VertexBarrelTrackerHits"],
+    "VXDHitCollection": VertexBarrelHitCollectionNames,
     "VXDDetectorName": ["VertexBarrel"],
     "pickUpSiHits": ["false"],
 }
