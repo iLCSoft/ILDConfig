0ffd3343959b29edbc4afb17f1398e5a736cc1cc
Victor Schwan <victor.schwan@cern.ch>
2026-08-04 15:41:14 +0200

    viewer fix: drop empty edm4hep::ParticleID colls

--- StandardConfig/production/ILDReconstruction.py
+++ StandardConfig/production/ILDReconstruction.py
@@ -352,6 +352,7 @@ if reco_args.lcioOutput != "only":
     output_commands.extend(get_drop_collections(CONSTANTS, True))
     # get_drop_collections incorrectly splits "type edm4hep::..." into two separate drops
     output_commands.append("drop type edm4hep::RecDqdxCollection")
+    output_commands.append("drop type edm4hep::ParticleIDCollection")
     io_handler.add_edm4hep_writer(f"{reco_args.outputFileBase}_REC.edm4hep.root", output_commands)
 
 
